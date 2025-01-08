6a484675d108ab4243eba79ade4ea51be7812476
PHIsixx <karsten.haensch@online.de>
2025-01-08 10:51:37 +0100

    removed unnecessary code from main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
+from sklearn.cluster import KMeans
 import torch
 
 from os.path import abspath, join, dirname
-from torch.utils.data import DataLoader
-
-from classes.streetsign_dataset import StreetsignDataset
 from utils.data_reader import get_training_testing_data_2
 from utils.tensors import load_image_tensors
 
 tensors_path = abspath(join(dirname(__file__), "..", "data", "tensors"))
 
-img_size = (25, 25)
+number_of_clusters = 6
+
 if __name__ == '__main__':
     # load precalculated tensors
     tensors = load_image_tensors(tensors_path)
@@ -17,9 +16,4 @@ if __name__ == '__main__':
     # get training and testing split with 0.9 training
     training, testing = get_training_testing_data_2(tensors, 0.9)
 
-    training_dataset = StreetsignDataset(training)
-    testing_dataset = StreetsignDataset(testing)
-
-
-    training_data_loader = DataLoader(training_dataset, batch_size=5, shuffle=True)
-    testing_data_loader = DataLoader(testing_dataset, batch_size=5, shuffle=True)
+    kmeans = KMeans(n_clusters=number_of_clusters)
